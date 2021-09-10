253fb11b157d3b531168196e4a9728441c752b4a
RamiHusari <rami1893@yahoo.com>
2021-09-10 12:28:51 +0200

    Bis Kapital 12.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { SearchComponent } from './search/search.component';
+import { TokenInterceptor } from './shared/token.interceptor';
+import { FormsModule } from '@angular/forms';
+import { DateValueAccessorModule } from 'angular-date-value-accessor';
+import { BookFormComponent } from './book-form/book-form.component';
+import { CreateBookComponent } from './create-book/create-book.component';
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppComponent,
+    BookListComponent,
+    BookListItemComponent,
+    BookDetailsComponent,
+    HomeComponent,
+    SearchComponent,
+    BookFormComponent,
+    CreateBookComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    DateValueAccessorModule,
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi:true}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/book-form/book-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit,Output,EventEmitter, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Book } from '../shared/book';
+import { BookFactory } from '../shared/book-factory';
+
+
+@Component({
+  selector: 'bm-book-form',
+  templateUrl: './book-form.component.html',
+  styleUrls: ['./book-form.component.css']
+})
+export class BookFormComponent implements OnInit {
+
+  book=BookFactory.empty();
+  @Output() submitBook = new EventEmitter<Book>();
+  @ViewChild('bookForm', {static : true}) bookForm : NgForm;
+
+
+  submitForm(){
+    this.submitBook.emit(this.book);
+
+      this.book = BookFactory.empty();
+      this.bookForm.reset();
+
+  }
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
